af9b279d0178af0c9a5db52f6f79cfe2d70db193
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-10-22 20:47:20 -0500

    create new article api route

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,8 +1,62 @@
 var express = require('express');
 var router = express.Router();
+var Articles = require('../../models/articles');
 
-router.get('/', function(req, res, next) {
-  res.json({success: true});
+/* GET articles listing. */
+router.get('/', function (req, res, next) {
+  Articles.find({}, function (err, articles) {
+    if (err) {
+      return res.json({
+        'success': false,
+        'error': err
+      });
+    }
+    return res.json({
+      'success': true,
+      'articles': articles
+    });
+  });
+});
+
+
+/* GET aticle by id */
+router.get('/:articleId', function (req, res) {
+  var articleId = req.params.articleId;
+  Articles.findOne({
+    '_id': articleId
+  }, function (err, article) {
+    if (err) {
+      return res.json({
+        'success': false,
+        'error': err
+      });
+    }
+    return res.json({
+      'success': true,
+      'article': article
+    });
+  });
+});
+
+
+/* POST new article */
+router.post('/', function (req, res) {
+  Articles.create(new Articles({
+    title: req.body.title,
+    slug: req.body.slug
+  }), function (err, article) {
+    if (err) {
+      return res.json({
+        'success': false,
+        article: req.body,
+        'error': err
+      });
+    }
+    return res.json({
+      success: true,
+      article: article
+    });
+  });
 });
 
 module.exports = router;
--- routes/articles.js
+++ routes/articles.js
@@ -1,41 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var Articles = require('../models/articles');
 
-/* GET articles listing. */
-router.get('/', function (req, res, next) {
-  Articles.find({}, function (err, articles) {
-    if (err) {
-      return res.json({
-        'success': false,
-        'error': err
-      });
-    }
-    return res.json({
-      'success': true,
-      'articles': articles
-    });
-  });
-});
-
-
-/* GET aticle by id */
-router.get('/:articleId', function (req, res) {
-  var articleId = req.params.articleId;
-  Articles.findOne({
-    '_id': articleId
-  }, function (err, article) {
-    if (err) {
-      return res.json({
-        'success': false,
-        'error': err
-      });
-    }
-    return res.json({
-      'success': true,
-      'article': article
-    });
-  });
+router.get('/', function(req, res, next) {
+  res.json({success: true});
 });
 
 module.exports = router;
