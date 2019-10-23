1c10ea620da55b5e640bd2af43844f0bae9537f3
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-10-22 19:34:06 -0500

    Connected to the database

---
+++ config.dev.js
@@ -0,0 +1,15 @@
+var config = {};
+config.mongodb = 'mongodb://localhost/articles';
+module.exports = config;
+
+// db schema
+// db.articles.insert({
+//   "title": "An example article",
+//   "slug": "",
+//   "description": "An article to show an object in this db",
+//   "keyword": "article",
+//   "body": "lorem ipsum",
+//   "published": "",
+//   "created": "",
+//   "modified": ""
+// })
