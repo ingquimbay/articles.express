6008e4eb5a041f54aca1abac8f7281055caa5b43
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-10-24 18:52:58 -0500

    Added a non-API endpoint for accessing the user app

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var Articles = require('../models/articles');
 
-router.get('/', function(req, res, next) {
-  res.json({success: true});
+router.get('/', function (req, res, next) {
+  res.json({
+    success: true
+  });
+});
+
+router.get('/', function (req, res, next) {
+  res.render('articles/index', {
+    title: 'xxx'
+  });
+});
+
+router.get('/:slug', function (req, res, next) {
+  res.render('articles/view', {
+    title: 'xxx'
+  });
+});
+
+router.get('/cms', function (req, res, next) {
+  res.render('articles/cms', {
+    title: 'CMS'
+  });
 });
 
 module.exports = router;
